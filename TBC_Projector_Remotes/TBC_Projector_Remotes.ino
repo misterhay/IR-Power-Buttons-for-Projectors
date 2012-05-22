1ee37b9931d3128d07bc7eb7deb1e1a48a6b2c9b
David Hay <misterhay@gmail.com>
2012-05-21 22:37:15 -0600

    Update TBC_Projector_Remotes/TBC_Projector_Remotes.ino

--- TBC_Projector_Remotes/TBC_Projector_Remotes.ino
+++ TBC_Projector_Remotes/TBC_Projector_Remotes.ino
@@ -1,23 +1,40 @@
 #include <IRremote.h>
+#include <Bounce.h>
 
-IRsend irsend;
+IRsend irsend; // the IR transmit pin is 10, also requires a 1k resistor
 
-const int buttonPin = 20; //The "power" button is on pin 20 and ground
+const int buttonPin = 20; //The button is on pin 20 and ground
+const int ledPin = 15; //The LED inside the button is pin 15
+int brightness = 30; //a variable for the LED inside the button
+int fadeamount = 1;
+
+Bounce pushbutton = Bounce(buttonPin, 10);  // 10 ms debounce
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP); //set as input with pullup resistor
+  pinMode(ledPin, OUTPUT);
 }
 
+
 void loop() {
-if (digitalRead(buttonPin)) {
- {
-   }
- else {
-    irsend.sendNEC(0xC40BF, 20); //send Benq power button signal
-    delay(100);
-    irsend.sendNEC(0xCF300FF, 20); //send Sanyo power button pressed
-    delay(100);
-    irsend.sendNEC(0xC73FF00, 20); //send Sanyo power button released
-    delay(100);
-     }
+  pushbutton.update ( ); // Update the debouncer 
+  int value = pushbutton.read(); // Get the update value
+  
+  if ( value == HIGH) {
+    //pulse the LED inside the button 
+     analogWrite(ledPin, brightness);
+     brightness = brightness + fadeamount;
+     if (brightness == 30 || brightness == 255) {
+        fadeamount = -fadeamount;
+        }
+     delay(20);      
+  }
+  else {
+     analogWrite(ledPin, 0);
+     irsend.sendNEC(0xC40BF, 32); //send Benq power button signal
+     delay(100);
+     irsend.sendNEC(0xCF300FF, 32); //send Sanyo power button pressed
+     delay(100);
+     irsend.sendNEC(0xC73FF00, 32); //send Sanyo power button released
+  }
 }
