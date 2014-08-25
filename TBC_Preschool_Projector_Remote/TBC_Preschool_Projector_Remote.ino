a4d0ddb5f685952372e7116861f1069f6ee6d221
David Hay <misterhay@gmail.com>
2014-08-24 23:22:46 -0600

    broke some things out into functions

--- TBC_Preschool_Projector_Remote/TBC_Preschool_Projector_Remote.ino
+++ TBC_Preschool_Projector_Remote/TBC_Preschool_Projector_Remote.ino
@@ -7,50 +7,47 @@
 #include <Bounce.h>
 
 IRsend irsend; // the IR transmit pin is 11, also requires a 50-150 ohm resistor
-const int button1 = 5; //The power button on this pin and ground
-const int button2 = 6; //The NoShow button on this pin and ground
-const int led1 = 14; //The LED inside the glowing power button
+const int powerButton = 5; //The power button on this pin and ground
+const int inputButton = 6; //The input button on this pin and ground
+const int led1 = 14; //The "breathing" LED
 const int led2 = 2; //The red status LED
 
-int brightness = 30; //variables for the LED inside the button
+int brightness = 30; //variables for the "breathing" LED
 int fadeamount = 1;
 
-Bounce bouncer1 = Bounce(button1, 20); // 20 millisecond debounce
-Bounce bouncer2 = Bounce(button2, 20);
+Bounce bouncer1 = Bounce(powerButton, 20); // 20 millisecond debounce
+Bounce bouncer2 = Bounce(inputButton, 20);
 
 void setup() {
-  pinMode(button1,INPUT_PULLUP);
-  pinMode(button2,INPUT_PULLUP);
+  pinMode(powerButton,INPUT_PULLUP);
+  pinMode(inputButton,INPUT_PULLUP);
   pinMode(led1,OUTPUT);
   pinMode(led2,OUTPUT);
-//  bouncer1.write(0);  // set the button pressed state as false so we don't turn on the projectors right away
-//  bouncer2.write(0);
+  bouncer1.write(1);  // set the button pressed state as false so we don't turn on the projectors right away
+  bouncer2.write(1);
+}
+
+void sendIR(int buttonPressed) {
+ analogWrite(led1, 0); // turn off the led so we know something is happening
+ digitalWrite(led2, HIGH); // turn on the red status LED so we know something is happening
+ if (buttonPressed == 1) {irsend.sendNEC(0xCF318E7, 32);} // send the power button signal
+ if (buttonPressed == 2) {irsend.sendNEC(0xCF310EF, 32);} // send the input button signal
+ digitalWrite(P2_3, LOW); // turn off the IR LED when not transmitting
+ delay(1000); // wait for a second
+}
+
+void ledBreathe() {
+ digitalWrite(led2, LOW); // turn the red status LED off
+ analogWrite(led1, brightness);
+ brightness = brightness + fadeamount;
+ if (brightness == 30 || brightness == 255) {fadeamount = -fadeamount;}
+ delay(20);
 }
 
 void loop() {
  bouncer1.update ( );  // Update the debouncer
  bouncer2.update ( );
-
- if (bouncer1.read() == 0) {
-     analogWrite(led1, 0); // turn off the led so we know something is happening
-     digitalWrite(led2, HIGH); // turn on the red status LED so we know something is happening
-     irsend.sendNEC(0xC40BF, 32); // send Benq power button signal
-   digitalWrite(P2_3, LOW); // turn off the IR LED when not transmitting
-     delay(1000); // wait for a second
- }
- else if (bouncer2.read() == 0) {
-     analogWrite(led1, 0); // turn off the led so we know something is happening
-     digitalWrite(led2, HIGH); // turn on the red status LED so we know something is happening
-     irsend.sendNEC(0xCF310EF, 32); // send Benq Source command
-   digitalWrite(P2_3, LOW); // turn off the IR LED when not transmitting
-     delay(1000); // wait for a second
- }
- else {
-   digitalWrite(led2, LOW); // turn the red status LED back off
-     //pulse the LED inside the button 
-     analogWrite(led1, brightness);
-     brightness = brightness + fadeamount;
-     if (brightness == 30 || brightness == 255) {fadeamount = -fadeamount;}
-     delay(20); 
- }
+ if (bouncer1.read() == 0) {sendIR(1);}
+ else if (bouncer2.read() == 0) {sendIR(2);}
+ else {ledBreathe();}
 }
